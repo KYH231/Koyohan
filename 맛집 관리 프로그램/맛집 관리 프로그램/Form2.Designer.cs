24c5911585de586eed7ca67a7304fcd3f3ffd285
요한 고 <yohanko2001@naver.com>
2023-12-07 23:03:07 +0900

    로그인 기능 추가

--- 맛집 관리 프로그램/맛집 관리 프로그램/Form2.Designer.cs
+++ 맛집 관리 프로그램/맛집 관리 프로그램/Form2.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tb1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -45,6 +45,7 @@
             this.button1.TabIndex = 9;
             this.button1.Text = "확인";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
@@ -54,14 +55,16 @@
             this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(241, 21);
             this.textBox2.TabIndex = 7;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
             // 
-            // textBox1
+            // tb1
             // 
-            this.textBox1.Location = new System.Drawing.Point(61, 35);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(241, 21);
-            this.textBox1.TabIndex = 8;
+            this.tb1.Location = new System.Drawing.Point(61, 35);
+            this.tb1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(241, 21);
+            this.tb1.TabIndex = 8;
+            this.tb1.Click += new System.EventHandler(this.tb1_Click);
             // 
             // label2
             // 
@@ -90,7 +93,7 @@
             this.ClientSize = new System.Drawing.Size(335, 158);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
@@ -104,7 +107,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tb1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
     }
--- 맛집 관리 프로그램/맛집 관리 프로그램/Form2.cs
+++ 맛집 관리 프로그램/맛집 관리 프로그램/Form2.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace 맛집_관리_프로그램
 {
     public partial class Form2 : Form
     {
-        public Form2()
+        private Form1 _Form1;
+        public Form2(Form1 form1)
         {
+            _Form1 = form1;
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bt_OK();
+        }
+        private void bt_OK()
+        {
+            if (tb1.Text.Equals("wp2") && textBox2.Text.Equals("1234"))
+            {
+                _Form1.LoginCheck = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("아이디 또는 패스워드가 잘못 입력되었습니다.");
+
+                tb1.Text = "아이디를 입력 해주세요.";
+                textBox2.Text = "비밀번호를 입력 해주세요.";
+                textBox2.PasswordChar = '\0';
+                tb1.Focus();
+            }
+        }
+
+        private void tb1_Click(object sender, EventArgs e)
+        {
+            ID_click();
+        }
+        private void ID_click()
+        {
+            if (tb1.Text == "아이디를 입력 해주세요.") tb1.Text = "";
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "비밀번호를 입력 해주세요.")
+            {
+                textBox2.Text = "";
+                textBox2.PasswordChar = '\0';
+            }
         }
     }
 }
